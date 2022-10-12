e97edf3056a1a4fa01daeaf27d64918fec71b8d3
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-12 18:04:11 +0200

    1 file modified, 1 added

--- src/pages/Admin/pages/AdminRecipes/AdminRecipes.js
+++ src/pages/Admin/pages/AdminRecipes/AdminRecipes.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import AdminRecipesNav from './components/AdminRecipesNav/AdminRecipesNav';
 
 function AdminRecipes() {
   return (
-    <>
-      <h3>Admin Recipes</h3>
-      <Outlet />
-    </>
+    <div className="d-flex flex-column flex-fill">
+      <h4 className="mb-20">Gestion des recettes</h4>
+      <div className="flex-fill d-flex flex-column">
+        <AdminRecipesNav></AdminRecipesNav>
+        <div className="flex-fill d-flex flex-column">
+          <Suspense>
+            <Outlet />
+          </Suspense>
+        </div>
+      </div>
+    </div>
   );
 }
 
